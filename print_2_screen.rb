cdf0621ca3ecdb29d508a2f9361ede5a78547fd8
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-06-06 14:58:48 -0700

    add notes

--- print_2_screen.rb
+++ print_2_screen.rb
@@ -10,17 +10,19 @@
 # 	number = number + 1
 # end
 
- num = 1
+  num = 1  #variable for number 1
   
-   100.times do
- 	if num % 15 == 0
+   100.times do  # how many times to run
+ 	if num % 15 == 0  # for every 15, print out go shakka.com
  		puts 'go shakka.com'
- 	elsif num % 3 == 0
+ 	elsif num % 5 == 0  #for increment of 5 say 'is cool'
     puts 'is cool'
-    elsif num % 5 == 0
+    elsif num % 3 == 0  #for increment of 3 say 'beginners luck'
     	puts 'beginners luck'
     else 
     	puts num
      end
      num = num + 1
      end
+ 
+ README.md 
