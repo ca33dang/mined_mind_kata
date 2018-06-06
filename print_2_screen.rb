a102fdb1bae0032ea5f954d667087bff185abfe3
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-06-06 14:06:30 -0700

    replace 3s with mined and 5s with minds

--- print_2_screen.rb
+++ print_2_screen.rb
@@ -10,12 +10,15 @@
 # 	number = number + 1
 # end
 
-num = 1
+ num = 1
   
-  100.times do
-	if num == 3
-		puts 'mined'
-	end
- puts num
- num = num + 1
- end
+   100.times do
+ 	if num % 3 == 0
+ 		puts 'go shakka.com'
+ 	elsif num % 5 == 0
+    puts 'is cool'
+    else
+     puts num
+     end 
+     num = num + 1
+     end
