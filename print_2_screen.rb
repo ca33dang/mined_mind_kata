8bc3153a3e3aed602cf8906347fd8e4e0b681902
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-06-06 12:18:19 -0700

    first commit

---
+++ print_2_screen.rb
@@ -0,0 +1,21 @@
+# p 1
+# puts 1
+# print 1
+# 100.times do
+# 	puts 1
+# end
+# number = 1
+# 100.times do
+# 	p number
+# 	number = number + 1
+# end
+
+num = 1
+  
+  100.times do
+	if num == 3
+		puts 'mined'
+	end
+ puts num
+ num = num + 1
+ end
