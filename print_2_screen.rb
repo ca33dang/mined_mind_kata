eaeff345a757a917fb4e42d6f0955e8de0dc699d
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-06-06 14:26:22 -0700

    change 15 to beginners luck

--- print_2_screen.rb
+++ print_2_screen.rb
@@ -13,12 +13,14 @@
  num = 1
   
    100.times do
- 	if num % 3 == 0
+ 	if num % 15 == 0
  		puts 'go shakka.com'
- 	elsif num % 5 == 0
+ 	elsif num % 3 == 0
     puts 'is cool'
-    else
-     puts num
-     end 
+    elsif num % 5 == 0
+    	puts 'beginners luck'
+    else 
+    	puts num
+     end
      num = num + 1
      end
